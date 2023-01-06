33b5400b2e3c8b2cdaf57d2eaab0431ff4456ff3
Sumer Patel <patelsum@oregonstate.edu>
2023-01-06 17:11:39 +0530

    added svelte components

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { fade, scale } from "svelte/transition";
+
+    import FeedbackItem from "./FeedbackItem.svelte";
+
+    export let feedback = [];
+</script>
+
+{#each feedback as feed (feed.id)}
+    <div in:scale out:fade="{{ duration: 400 }}">
+        <FeedbackItem item={feed} on:delete-feedback />
+    </div>
+{/each}
